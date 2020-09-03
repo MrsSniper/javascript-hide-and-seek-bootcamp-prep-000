cff0ed796900d9a41998b11627e2f250e9188401
Nessa S <bnvsnipe@gmail.com>
2020-09-03 23:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(n + i);
+  }
 
+}
